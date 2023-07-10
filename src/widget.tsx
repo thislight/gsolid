04da451370d03236731a339d7a097052973ed777
thislight <l1589002388@gmail.com>
2023-07-10 23:24:38 +0800

    added forwardRef

--- src/widget.tsx
+++ src/widget.tsx
@@ -7,11 +7,63 @@ import {
     JSX,
     spreadProps,
     ComponentProps,
-    createRenderEffect,
     ValidComponent,
     createMemo,
     untrack,
 } from "./index.js";
+
+/**
+ * Forward reference to the parent component.
+ *
+ * Only call this function once you actually got the reference!
+ *
+ * Do:
+ * ````ts
+ * const Spam = (props) => {
+ *  const widget = new Gtk.Widget({})
+ *  forwardRef(widget, props.ref)
+ *  return widget
+ * }
+ * ````
+ *
+ * Don't:
+ * ````jsx
+ * const Foo = (props) => {
+ *  let ref: Gtk.Widget
+ *  forwardRef(ref, props.ref) // The ref is undefined at the moment!
+ *  return <Widget Widget={Gtk.Widget} ref={ref!} />
+ * }
+ * ````
+ * 
+ * Don't:
+ * ````jsx
+ * const Foo = (props) => {
+ *  let ref: Gtk.Widget
+ *  onMount(() => forwardRef(ref, props.ref))
+ *  return <Widget Widget={Gtk.Widget} ref={ref!} />
+ * }
+ * ````
+ * 
+ * onMount's callback is run after the render phase. It won't work as intended.
+ * 
+ * Instead:
+ * ````jsx
+ * const Foo = (props) => {
+ *  let ref: Gtk.Widget
+ *  return <Widget Widget={Gtk.Widget} ref={(r) => ref = forwardRef(r, props.ref)} />
+ * }
+ * ````
+ *
+ * @param ref
+ * @param forwardTo
+ */
+export function forwardRef<T>(ref: T, forwardTo?: Ref<T>): T {
+    if (typeof forwardTo == "function") {
+        // the ref passing in component is always a function
+        (forwardTo as (ref: T) => void)(ref);
+    }
+    return ref
+}
 
 interface WidgetProps<T extends JSX.Element> {
     ref?: Ref<T>;
@@ -21,30 +73,31 @@ interface WidgetProps<T extends JSX.Element> {
 
 /**
  * Create Gtk widgets as components.
+ *
+ * If you need a ref of the widget, consider {@link Widget}.
+ *
  * @param widgetClass the widget constructor
  * @param props properties
- * @returns 
+ * @returns
  */
 export function useWidget<T extends JSX.Element>(
     widgetClass: new () => T,
     props: { ref?: Ref<T>; [key: string]: unknown }
-) {
+): T {
     const node = new widgetClass();
     spreadProps(node, props);
-    /* Patch to work with `ref` */
-    if (props.ref) {
-        const ref = props.ref;
-        if (typeof ref == "function") {
-            createRenderEffect(() => ref(node))
-        }
-    }
+    forwardRef(node, props.ref);
     return node;
 }
 
 /**
  * This component wraps GTK widgets as a component.
+ *
+ * If you don't need a ref of the widget, consider {@link useWidget}.
+ *
+ * You can use {@link forwardRef} to forward references.
  */
-export function Widget<T extends JSX.Element>(props: WidgetProps<T>) {
+export function Widget<T extends JSX.Element>(props: WidgetProps<T>): T {
     const [{ Widget: Klass }, rest] = splitProps(props, ["Widget"]);
     return useWidget(Klass, rest);
 }
@@ -56,7 +109,7 @@ export type DynamicProps<T extends ValidComponent, P = ComponentProps<T>> = {
 };
 
 /**
- *
+ * renders an arbitrary custom or native component and passes the other props
  * @param props
  * @returns
  */
@@ -67,6 +120,34 @@ export function Dynamic<T extends ValidComponent>(
     const cache = createMemo(() => p.component);
     return createMemo(() => {
         const component = cache();
-        return untrack(() => component(rest));
-    }) as unknown as JSX.Element; // This can work under jsx context
+        return untrack<JSX.Element>(() => component(rest));
+    }) as unknown as JSX.Element;
+}
+
+/**
+ * Test if two widget or widget array is equal.
+ *
+ * If `a` and `b` are arrays, checks if two arrays have same lengths and same elements.
+ *
+ * Equality test is `===`.
+ *
+ * @param a widget or widget array
+ * @param b widget or widget array
+ * @returns
+ */
+export function widgetEquals(a: unknown, b: unknown) {
+    if (Array.isArray(a) && Array.isArray(b)) {
+        if (a.length == b.length) {
+            for (let i = 0; i < a.length; i++) {
+                if (a[i] !== b[i]) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return a === b;
+    }
 }
