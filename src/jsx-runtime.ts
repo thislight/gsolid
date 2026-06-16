62604a67defab2c6a0c3bac55ccb7962b3ca8ecd
rubicon <rubicon@lightstands.xyz>
2026-06-16 16:02:16 +0800

    jsx-runtime: JSX.Element no longer accepts Gtk.Widget[]
    * Widget[] and Widget is completely different in Gtk
    * use JSX.ArrayElement if you want to accepts array of elements

--- src/jsx-runtime.ts
+++ src/jsx-runtime.ts
@@ -1,8 +1,8 @@
 /**
  * @license Apache-2.0
- * 
+ *
  * This module is the JSX runtime to be imported as transpiler translating JSX syntax.
- * 
+ *
  * Normally It's not meant to be used by users.
  * @module
  */
@@ -12,100 +12,104 @@ import GObject from "gi://GObject";
 import type { Accessor, Component } from "./index.js";
 
 export declare namespace JSX {
-    type Element = Gtk.Widget | Gtk.Widget[] | undefined | null;
-    type ArrayElement = Element[];
-    type ElementType = Element | ArrayElement | Component<any> | Accessor<Element>
+  type Element = Gtk.Widget | undefined;
+  type ArrayElement = Element[];
+  type ElementType =
+    | Element
+    | ArrayElement
+    | Component<any>
+    | Accessor<Element>;
 
-    interface IntrinsicElements {}
+  interface IntrinsicElements {}
 }
 
 function camelToKecab(str: string) {
-    if (str !== str.toLowerCase()) {
-        const newStr = str.replace(/[A-Z]/g, (c) => `-${c.toLowerCase()}`);
-        if (/^[A-Z]+/.test(str)) {
-            return newStr.slice(1); // Remove the first '-' if the original is big camel
-        } else {
-            return newStr;
-        }
+  if (str !== str.toLowerCase()) {
+    const newStr = str.replace(/[A-Z]/g, (c) => `-${c.toLowerCase()}`);
+    if (/^[A-Z]+/.test(str)) {
+      return newStr.slice(1); // Remove the first '-' if the original is big camel
+    } else {
+      return newStr;
     }
-    return str;
+  }
+  return str;
 }
 
 export const {
-    effect,
-    memo,
-    createComponent,
-    createElement,
-    createTextNode,
-    insertNode,
-    insert,
-    spread,
-    setProp,
-    mergeProps,
+  effect,
+  memo,
+  createComponent,
+  createElement,
+  createTextNode,
+  insertNode,
+  insert,
+  spread,
+  setProp,
+  mergeProps,
 } = createRenderer<Gtk.Widget>({
-    createElement(name) {
-        throw new TypeError(`could not create element for ${name}`);
-    },
-    createTextNode(value) {
-        // FIXME: I don't known exactly if we need TextNode in gtk...
-        // If we can avoid the case (since we don't need such thing in gtk),
-        // throw an error is better than make a Label
-        // Note: leave comment here if you understand this thing
-        throw new TypeError(
-            "working on a text node, this might be a bug since we don't need the thing in gtk"
+  createElement(name) {
+    throw new TypeError(`could not create element for ${name}`);
+  },
+  createTextNode(value) {
+    // FIXME: I don't known exactly if we need TextNode in gtk...
+    // If we can avoid the case (since we don't need such thing in gtk),
+    // throw an error is better than make a Label
+    // Note: leave comment here if you understand this thing
+    throw new TypeError(
+      "working on a text node, this might be a bug since we don't need the thing in gtk",
+    );
+  },
+  replaceText(textNode, value) {
+    throw new TypeError(
+      "working on a text node, this might be a bug since we don't need the thing in gtk",
+    );
+  },
+  setProperty(node, name, value, prev) {
+    if (name.startsWith("on")) {
+      const originalName = name.slice(2);
+      const convertedName = originalName.startsWith(":")
+        ? originalName.slice(1) // support on:any-signal-name=___
+        : camelToKecab(originalName);
+      if (typeof value !== "function") {
+        throw new TypeError("Signal recevier must be a function");
+      }
+      if (prev) {
+        GObject.signal_handlers_disconnect_by_func(
+          node,
+          prev as (...rest: any[]) => any,
         );
-    },
-    replaceText(textNode, value) {
-        throw new TypeError(
-            "working on a text node, this might be a bug since we don't need the thing in gtk"
-        );
-    },
-    setProperty(node, name, value, prev) {
-        if (name.startsWith("on")) {
-            const originalName = name.slice(2);
-            const convertedName = originalName.startsWith(":")
-                ? originalName.slice(1) // support on:any-signal-name=___
-                : camelToKecab(originalName);
-            if (typeof value !== "function") {
-                throw new TypeError("Signal recevier must be a function");
-            }
-            if (prev) {
-                GObject.signal_handlers_disconnect_by_func(
-                    node,
-                    prev as (...rest: any[]) => any
-                );
-            }
-            node.connect(convertedName, value as (...args: any[]) => any);
-        } else {
-            const convertedName = name.startsWith("prop:")
-                ? name.slice(5) // support prop:AnyPropertyName=___
-                : camelToKecab(name);
-            node.set_property(convertedName, value);
-        }
-    },
-    insertNode(parent, node, anchor) {
-        node.insert_before(parent, anchor || null);
-    },
-    isTextNode(node) {
-        return false;
-    },
-    removeNode(parent, node) {
-        let child = parent.get_first_child();
-        while (child) {
-            if (child === node) {
-                child.unparent();
-                break;
-            }
-            child = child.get_next_sibling();
-        }
-    },
-    getParentNode(node) {
-        return node.get_parent() || undefined;
-    },
-    getFirstChild(node) {
-        return node.get_first_child() || undefined;
-    },
-    getNextSibling(node) {
-        return node.get_next_sibling() || undefined;
-    },
+      }
+      node.connect(convertedName, value as (...args: any[]) => any);
+    } else {
+      const convertedName = name.startsWith("prop:")
+        ? name.slice(5) // support prop:AnyPropertyName=___
+        : camelToKecab(name);
+      node.set_property(convertedName, value);
+    }
+  },
+  insertNode(parent, node, anchor) {
+    node.insert_before(parent, anchor || null);
+  },
+  isTextNode(node) {
+    return false;
+  },
+  removeNode(parent, node) {
+    let child = parent.get_first_child();
+    while (child) {
+      if (child === node) {
+        child.unparent();
+        break;
+      }
+      child = child.get_next_sibling();
+    }
+  },
+  getParentNode(node) {
+    return node.get_parent() || undefined;
+  },
+  getFirstChild(node) {
+    return node.get_first_child() || undefined;
+  },
+  getNextSibling(node) {
+    return node.get_next_sibling() || undefined;
+  },
 });
