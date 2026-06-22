1137d53f600949a2518e2ac05c3acc9fb0741d3a
rubicon <rubicon@lightstands.xyz>
2026-06-22 15:33:24 +0800

    web-ponyfill: rewrite to match the Web API behaviour

--- src/web-ponyfill.ts
+++ src/web-ponyfill.ts
@@ -1,16 +1,34 @@
 /**
  * This module includes some API implementations, which is not exists in gjs, for solid js.
- * 
+ *
  * This module injects into solid js by a bundler, and is not intended to be used by user code.
- * 
+ *
  * @license Apache-2.0
  * @module
  */
 import GLib from "gi://GLib";
 
+const microtaskBatch: (() => void)[] = [];
+let microtaskScheduleId: number | undefined = undefined;
+
+function runMicrotasks() {
+  try {
+    let nextMicrotask;
+    while ((nextMicrotask = microtaskBatch.shift())) {
+      nextMicrotask();
+    }
+  } finally {
+    microtaskScheduleId = undefined;
+  }
+  return false;
+}
+
 export function queueMicrotask(fn: () => void) {
-    GLib.timeout_add(GLib.PRIORITY_LOW, 0, () => {
-        fn();
-        return false;
-    });
+  microtaskBatch.push(fn);
+  if (!microtaskScheduleId) {
+    microtaskScheduleId = GLib.idle_add(
+      GLib.PRIORITY_DEFAULT_IDLE,
+      runMicrotasks,
+    );
+  }
 }
